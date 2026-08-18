ff921fb1b130b9c3060cf75b64694a4f562e3f9b
sebillanoangelo <sebillanoangelo8@gmail.com>
2026-08-18 14:53:23 +0800

    Create Student Profile.c

---
+++ Student Profile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("========================================\n");
+    printf("           STUDENT PROFILE\n");
+    printf("========================================\n");
+    printf("Name: Angelo M. Sebillano\n");
+    printf("Course and Section: BSIT-1R13\n");
+    printf("Year Level: 1st Year\n");
+    printf("========================================\n");
+    printf("           WEB DEVELOPER\n");
+    printf("========================================\n");
+
+    return 0;
+}
