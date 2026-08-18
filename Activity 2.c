28d181b879687e71ba7aa8a436d2e1a1ae6629c1
sebillanoangelo <sebillanoangelo8@gmail.com>
2026-08-18 15:30:25 +0800

    Create Activity 2.c

---
+++ Activity 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("============================================\n");
+    printf("=                                          =\n");
+    printf("=         WELCOME TO PROGRAMMING           =\n");
+    printf("=                                          =\n");
+    printf("=                                          =\n");
+    printf("=                                          =\n");
+    printf("=   Sleep  *  School  *  Play  *  Sleep    =\n");
+    printf("=                                          =\n");
+    printf("=                                          =\n");
+    printf("=                                          =\n");
+    printf("============================================\n");
+
+    return 0;
+}
